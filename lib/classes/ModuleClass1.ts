b538576ab8ede084a9b85cc39f2a9c7c89561202
wsalame <wsalame89@gmail.com>
2014-12-01 21:00:20 -0500

    initial commit

---
+++ lib/classes/ModuleClass1.ts
@@ -0,0 +1,5 @@
+class ModuleClass1 implements mkmymodule.ModuleClass1 {
+  value: string;
+  id: number;
+}
+export = ModuleClass1;
---
+++ typings/tsd.d.ts
@@ -0,0 +1,11 @@
+/// <reference path="node/node.d.ts" />
+/// <reference path="mykoop/mykoop.d.ts" />
+/// <reference path="mykoop-database/mykoop-database.d.ts" />
+/// <reference path="../node_modules/mykoop-utils/typings/mykoop-utils/mykoop-utils.d.ts" />
+/// <reference path="../node_modules/mykoop-logger/typings/mykoop-logger/mykoop-logger.d.ts" />
+/// <reference path="express/express.d.ts" />
+/// <reference path="async/async.d.ts" />
+/// <reference path="mysql/mysql.d.ts" />
+
+// FIXME::CHANGE THIS TO NEW MODULE NAME
+/// <reference path="mykoop-mymodule/mykoop-mymodule.d.ts" />
