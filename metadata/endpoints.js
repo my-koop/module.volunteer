4c02ec271c2bdeb6e902c0ba5528c4ac0eaa1fbe
wsalame <wsalame89@gmail.com>
2014-12-01 22:28:33 -0500

    Endpoints avaibilities

--- metadata/endpoints.js
+++ metadata/endpoints.js
@@ -1,2 +1,37 @@
-var endpoints = {};
+var endpoints = {
+    availability: {
+        list: {
+            path: "/availability/list",
+            method: "get"
+        },
+        listByUser: {
+            path: "/availability/user/:idUser/list",
+            method: "get"
+        },
+        update: {
+            path: "/availability/update/",
+            method: "post",
+            validation: {
+                resolve: "validation",
+                value: "availabilityObject"
+            }
+        },
+        add: {
+            path: "/availability/",
+            method: "post",
+            validation: {
+                resolve: "validation",
+                value: "availabilityObject"
+            }
+        },
+        get: {
+            path: "/availability/:id",
+            method: "get"
+        },
+        remove: {
+            path: "/availability/:id",
+            method: "delete"
+        }
+    }
+};
 module.exports = endpoints;
