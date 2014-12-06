af6d0d83892b98ac35354c87c60114072d8c973f
wsalame <wsalame89@gmail.com>
2014-12-05 19:48:27 -0500

    Class availability

---
+++ lib/classes/Availability.ts
@@ -0,0 +1,21 @@
+class Availability implements mkvolunteer.Availability {
+  public  id                  : number;
+  public  idUser              : number;
+  public  firstName           : string;
+  public  lastName            : string;
+  public  startDate           : Date;
+  public  endDate             : Date;
+  public  fullName            : string;
+
+  constructor(row: any) {
+    this.id                  = Number(row.idAvailability);
+    this.idUser              = Number(row.idUser);
+    this.startDate           = row.startDate;
+    this.endDate             = row.endDate;
+    this.firstName           = row.firstName;
+    this.lastName            = row.lastName;
+    this.fullName            = this.firstName + " " + this.lastName;
+  }
+}
+
+export = Availability;
